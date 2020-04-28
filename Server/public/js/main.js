c8fe3c5bec326f29beb114a00ca2869666f44b70
Claudio Sobrinho <clf.regis@gmail.com>
2020-04-28 16:18:24 -0300

    Change from Chart.js to ZingChart adding zoom features

--- Server/public/js/main.js
+++ Server/public/js/main.js
@@ -1,5 +1,5 @@
-var temp = [], hum = [], time = [], state = [], xlabel = [], daily = [], continuous = [];
-var status = 'OFF';
+var temp = [], hum = [], time = [], state = [], daily = [], continuous = [];
+var status;
 var motor_label = document.getElementById("motorLabel").textContent;
 var current_motor = 'Motor 1';
 var yStatusLabels = {
@@ -24,7 +24,7 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
 var database = firebase.database().ref(motor_label);
 
-firebase.database.enableLogging(true);
+//firebase.database.enableLogging(true);
 
 database.on('value', function(snapshot) {
   let snap = snapshot.val(); // vai ter todo o objeto
@@ -37,36 +37,31 @@ database.on('value', function(snapshot) {
         hum.push(snap[i][n]);
       }
       if (n=='D'){
-        const tmp1 = snap[i][n].split(' ');
-        const tmp2 = snap[i][n].split('');
-        xlabel.push(tmp1[0]+", "+tmp1[1]+" "+tmp1[2]+" "+tmp1[4]);
-        time.push(tmp2[11]+tmp2[12]+tmp2[13]+tmp2[14]+tmp2[15]);
+        time.push(snap[i][n]*1000);
       }
       if (n=='S'){
         state.push(snap[i][n]);
       }
       if (n=='DO'){
-        daily.push(format_hour(snap[i][n]));
+        daily.push(format_ISOhour(snap[i][n]));
       }
       if (n=='CO'){
-        continuous.push(format_hour(snap[i][n]));
+        continuous.push(format_ISOhour(snap[i][n]));
       }
     }
   }
   let current_temp = temp[temp.length - 1];
-  temp = temp.slice(-12, temp.length);
+  //temp = temp.slice(-12, temp.length);
   let current_hum = hum[hum.length - 1];
-  hum = hum.slice(-12, hum.length);
+  //hum = hum.slice(-12, hum.length);
   let current_time = time[time.length - 1];
-  time = time.slice(-12, time.lenght);
+  //time = time.slice(-12, time.lenght);
   let current_state = state[state.length - 1];
-  state = state.slice(-12, state.lenght);
-  let current_xlabel = xlabel[xlabel.length - 1];
-  xlabel = xlabel.slice(-12, xlabel.length);
+  //state = state.slice(-12, state.lenght);
   let current_daily = daily[daily.length - 1];
-  daily = daily.slice(-12, daily.length);
+  //daily = daily.slice(-12, daily.length);
   let current_continuous = continuous[continuous.length - 1];
-  continuous = continuous.slice(-12), daily.length;
+  //continuous = continuous.slice(-12), daily.length;
 
   if (current_state==1){
     status='Operating';
@@ -87,6 +82,9 @@ database.on('value', function(snapshot) {
   else if (motor_label=='motor_3'){
     current_motor = 'Motor 3';
   }
+  else if (motor_label=='motor_4'){
+    current_motor = 'Motor 4';
+  }
 
   document.getElementById("status").innerHTML = "Status: " + status;
   document.getElementById("nameMotor").innerHTML = "Name: " + current_motor;
@@ -94,264 +92,216 @@ database.on('value', function(snapshot) {
   document.getElementById("currentHumidity").innerHTML = "Humidity: " + current_hum + "%";
   document.getElementById("dailyOperation").innerHTML = "Daily: " + current_daily;
   document.getElementById("continuousOperation").innerHTML = "Continuous: " + current_continuous;
-  document.getElementById("lastUpdate").innerHTML = "Last Update: " + current_time + ", " + current_xlabel;
+  document.getElementById("lastUpdate").innerHTML = "Last Update: " + format_hour(current_time/1000) + ", " + format_date(current_time/1000);
 
-  drawTemperature(time, current_xlabel, temp);
-  drawHumidity(time, current_xlabel, hum);
-  drawGraphStatus(time, current_xlabel, state);
+  drawGraph(time, temp, 'Temperature [ºC]', 'Temperature', 'Temperature: %v ºC', 'spline', '#C6BD74', 45);
+  drawGraph(time, hum, 'Humidity [%]', 'Humidity', 'Humidity: %v %', 'spline', '#84A6CC', 100);
+  drawGraph(time, state, 'Operation', 'statusChart', 'Status: %v', 'stepped', '#689167', 1);
 });
 
-// function changeMotor(newMotor){
-//   document.getElementById("motorLabel").innerHTML = newMotor;
-// }
-
 function format_hour(seconds){
+  // https://www.geeksforgeeks.org/javascript-date-toisostring-function/
   var date = new Date(null);
-  date.setSeconds(seconds); // specify value for SECONDS here
+  date.setSeconds(seconds); // specify value for SECONDS here, from ESP32, UTC time
+  var result = date.toString().substr(16, 8); //toSring automatically adjust the TimeZone
+  return result;
+}
+
+function format_ISOhour(seconds){
+  // https://www.geeksforgeeks.org/javascript-date-toisostring-function/
+  var date = new Date(null);
+  date.setSeconds(seconds); // specify value for SECONDS here, from ESP32, delta time, no TIMEZONE required
   var result = date.toISOString().substr(11, 8);
   return result;
 }
 
-function drawTemperature(label, label_title, graph) {
-  var ctx = document.getElementById("Temperature").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: label,
-      datasets: [{
-        label: "Temperature",
-        labelString: "ºC",
-        borderColor: "#C6BD74",
-        backgroundColor: "#C6BD74",
-        borderWidth: 4,
-        fill: false,
-        data: graph,
-      }]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      aspectRatio: 1,
-      hoverMode: 'index',
-      stacked: false,
-      title: {
-        display: false,
-      },
-      legend: {
-        display: false,
-      },
-
-      scales: {
-        xAxes: [{
-          display: true,
-          gridLines: {
-            display: true,
-            color: '#676E7A',
-            zeroLineColor: '#676E7A',
-          },
-          ticks: {
-            fontSize: 14,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          },
-          scaleLabel: {
-            display: true,
-            labelString: label_title,
-            fontSize: 18,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          }
-        }],
-        yAxes: [{
-          type: "linear",
-          display: true,
-          position: "left",
-          gridLines: {
-            display: true,
-            color: '#676E7A',
-            zeroLineColor: '#676E7A',
-          },
-          ticks: {
-            beginAtZero: true,
-            stepSize: 1,
-            Max: 50,
-            suggestedMax: 45,
-            fontSize: 14,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'ºC',
-            fontSize: 18,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          }
-        }],
-      }
-    }
-  });
+function format_date(seconds){
+  // https://www.w3schools.com/js/js_dates.asp
+  var date = new Date(null);
+  date.setSeconds(seconds); // specify value for SECONDS here, from ESP32, UTC time
+  var result = date.toDateString(); //toSring automatically adjust the TimeZone
+  return result;
 }
 
-function drawHumidity(label, label_title, graph) {
-  var ctx = document.getElementById("Humidity").getContext('2d');
-  var myChart = new Chart(ctx, {
+function drawGraph(local_xValues, local_yValues, local_yLabel, local_ID, 
+  local_tooltipText, local_aspect, local_graphColor, yMaxValue) {
+  const chartConfig = {
     type: 'line',
-    data: {
-      labels: label,
-      datasets: [{
-        label: "Humidity",
-        labelString: "%",
-        borderColor: "#84A6CC",
-        backgroundColor: "#84A6CC",
-        borderWidth: 4,
-        fill: false,
-        data: graph,
-      }]
+    globals: {
+      backgroundColor: 'transparent',
+      fontFamily: 'Helvetica Neue',
+      fontColor: '#BBC1CB',
     },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      aspectRatio: 1,
-      hoverMode: 'index',
-      stacked: false,
-      title: {
-        display: false,
-      },
-      legend: {
-        display: false,
-      },
-
-      scales: {
-        xAxes: [{
-          display: true,
-          gridLines: {
-            display: true,
-            color: '#676E7A',
-            zeroLineColor: '#676E7A',
-          },
-          ticks: {
-            fontSize: 14,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          },
-          scaleLabel: {
-            display: true,
-            labelString: label_title,
-            fontSize: 18,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          }
-        }],
-        yAxes: [{
-          type: "linear",
-          display: true,
-          position: "left",
-          gridLines: {
-            display: true,
-            color: '#676E7A',
-            zeroLineColor: '#676E7A',
-          },
-          ticks: {
-            beginAtZero: true,
-            stepSize: 1,
-            Max: 100,
-            suggestedMax: 100,
-            Min: 0,
-            fontSize: 14,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          },
-          scaleLabel: {
-            display: true,
-            labelString: '%',
-            fontSize: 18,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          }
-        }],
-      }
-    }
-  });
-}
-  
-
-function drawGraphStatus(label, label_title, graph1) {
-  var ctx = document.getElementById("StatusChart").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: label,
-      datasets: [{
-        label: "Status",
-        borderColor: '#689167',
-        backgroundColor: '#689167',
-        borderWidth: 4,
-        lineTension: 0,
-        fill: false,
-        data: graph1,
-        steppedLine: true,
-      }]
+    plot: {
+      aspect: local_aspect
     },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      aspectRatio: 1,
-      hoverMode: 'index',
-      stacked: false,
-      title: {
-        display: false,
+    plotarea: {
+      marginTop: 10,
+      marginLeft: 60,
+      marginRight: 60,
+      marginBottom: 120,
+    },
+    scaleY: {
+      maxValue: yMaxValue,
+      minValue: 0,
+      step: 1,
+      tick: {
+        lineColor: '#676E7A',
+        lineStyle: 'solid',
+        lineWidth: '1px',
+        visible: true,
       },
-      legend: {
-        display: false,
+      guide: {
+        lineColor: '#676E7A',
+        lineStyle: 'solid',
+        lineWidth: '1px',
+        visible: true,
+        rules: [{
+          rule: "%i == 0",
+          visible: false
+        }]
       },
+      item: {
+        fontSize: 14,
+      },
+      label: {
+        text: local_yLabel,
+        bold: false,
+        fontSize: 18,
+      },
+      zooming:false
+    },
 
-      scales: {
-        xAxes: [{
-          display: true,
-          gridLines: {
-            display: true,
-            color: '#676E7A',
-            zeroLineColor: '#676E7A',
-          },
-          ticks: {
-            fontSize: 14,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          },
-          scaleLabel: {
-            display: true,
-            labelString: label_title,
-            fontSize: 18,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-          }
-        }],
-        yAxes: [{
-          type: "linear",
-          display: true,
-          position: "left",
-          gridLines: {
-            display: true,
-            color: '#676E7A',
-            zeroLineColor: '#676E7A',
-          },
-          ticks: {
-            beginAtZero: true,
-            stepSize: 1,
-            Max: 1,
-            fontSize: 14,
-            fontColor: '#BBC1CB',
-            fontFamily: 'Helvetica Neue',
-            callback: function(value, index, values) {
-              return yStatusLabels[value];
+    crosshairX: {
+      lineColor: '#BBC1CB',
+      lineStyle: 'dashed',
+      lineWidth: '1px',
+      marker: {
+        type: 'triangle',
+        size: '5px',
+        visible: true
+      },
+      plotLabel: {
+        visible: false
+      },
+      scaleLabel:{
+        backgroundColor: '#484F5D',
+        color: '#C6BD74',
+        borderColor: '#BBC1CB',
+        borderWidth: '1px',
+        fontSize: 14,
+      },
+    },
+
+    scaleX: {
+      labels: local_xValues,
+      step: "second",
+      transform: {
+        type: 'date',
+        all: '%D, %dd %M %Y<br>%H:%i:%s',
+      },
+      tick: {
+        lineColor: '#676E7A',
+        lineStyle: 'solid',
+        lineWidth: '1px',
+        visible: true,
+      },
+      guide: {
+        lineColor: '#676E7A',
+        lineStyle: 'solid',
+        lineWidth: '1px',
+        visible: true,
+        rules: [{
+          rule: "%i == 0",
+          visible: false
+        }]
+      },
+      item: {
+        fontSize: 14,
+      },
+      zooming: true,
+      zoomTo: [local_xValues.length-15,local_xValues.length],
+    },
+    tooltip: {
+      backgroundColor: '#484F5D',
+      color: '#C6BD74',
+      fontSize: 14,
+      text: local_tooltipText,
+    },
+    zoom:{
+      backgroundColor: '#C6BD74',
+      borderColor: '#BBC1CB',
+      borderWidth: 1,
+    },
+
+    preview: {
+      mask:{
+        backgroundColor: 'white',
+      },
+      handle: {
+        height: 30,
+        backgroundColor: 'white',
+      },
+      label: {
+        visible: false,
+      },
+      live: true,
+      
+    },
+    noData: {
+      text: 'No data found',
+      backgroundColor: 'transparent'
+    },
+    series: [
+          { 
+            'values': local_yValues,
+            'line-color' : local_graphColor,
+            'line-width' : 4,
+            marker: { /* Marker object */
+              'background-color': local_graphColor, /* hexadecimal or RGB value */
+              size : 4, /* in pixels */
+              'border-color' : local_graphColor, /* hexadecimal or RBG value */
+              'border-width' : 2 /* in pixels */
             }
-          },
-          scaleLabel: {
-            display: false
           }
-        }],
-      }
-    }
+        ]
+  };
+  zingchart.render({
+    id: local_ID,
+    data: chartConfig
   });
+
+  // Zoom Button Magic
+  // zingchart.loadModules('zoom-buttons', () => {
+  //   // fetch the data remotely
+  //   fetch('https://cdn.zingchart.com/datasets/timeseries-sample-data-2019.json')
+  //     .then(res => res.json())
+  //     .then(timeseriesData => {
+  //       // assign data
+  //       //chartConfig.series[0].values = timeseriesData.values;
+  //       // destroy the chart since we have to render the
+  //       // chart with a module. if there is no module,
+  //       // just use set data like the catch statement
+  //       zingchart.exec(local_ID, 'destroy');
+  //       // render chart with width and height to
+  //       // fill the parent container CSS dimensions
+  //       zingchart.render({
+  //         id: local_ID,
+  //         data: chartConfig,
+  //         modules: 'zoom-buttons'
+  //       });
+  //     })
+  //     .catch(e => {
+  //       // if error, render blank chart
+  //       console.error('--- error fetching data from: https://cdn.zingchart.com/datasets/timeseries-sample-data.json ---');
+  //       chartConfig.title = {};
+  //       chartConfig.title.text = 'Error Fetching https://cdn.zingchart.com/datasets/timeseries-sample-data.json';
+  //       // just exec setdata api method since we don't need to render the zoom modules
+  //       // https://www.zingchart.com/docs/api/methods/
+  //       zingchart.exec(local_ID, 'setdata', {
+  //         data: chartConfig
+  //       });
+  //     });
+  // });
+
 }
